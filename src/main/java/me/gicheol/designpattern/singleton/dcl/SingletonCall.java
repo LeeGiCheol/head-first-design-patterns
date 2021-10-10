42011e1e46b1ad08e13abe2af385a38aceb30c9d
LeeGiCheol <asdqwek200@naver.com>
2021-10-10 12:31:23 +0900

    volatile 사용해서 Singleton 객체 생성

---
+++ src/main/java/me/gicheol/designpattern/singleton/dcl/SingletonCall.java
@@ -0,0 +1,11 @@
+package me.gicheol.designpattern.singleton.dcl;
+
+
+public class SingletonCall {
+
+    public static void main(String[] args) {
+//        Singleton ingleton = new Singleton(); private 생성자이기 때문에 new를 통해 객체 생성 불가
+        Singleton instance = Singleton.getInstance();
+    }
+
+}
